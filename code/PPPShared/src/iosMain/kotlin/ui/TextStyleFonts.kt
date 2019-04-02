cd51441481e9bab4037a3cf334ca8d46cac9358e
Ellen Shapiro <designatednerd@gmail.com>
2019-04-02 14:17:13 +0200

    fix float value that no longer exists

--- code/PPPShared/src/iosMain/kotlin/ui/TextStyleFonts.kt
+++ code/PPPShared/src/iosMain/kotlin/ui/TextStyleFonts.kt
@@ -1,10 +1,9 @@
 package no.bakkenbaeck.pppshared.ui
 
 import platform.UIKit.UIFont
-import no.bakkenbaeck.pppshared.cgFloatValue
 
 fun TextStyle.toUIFont(): UIFont {
-    val fontSize = this.fontSize.defaultPoints.cgFloatValue
+    val fontSize = this.fontSize.defaultPoints.toDouble()
     return when (this.fontStyle) {
         FontStyle.Regular -> UIFont.systemFontOfSize(fontSize)
         FontStyle.Bold -> UIFont.boldSystemFontOfSize(fontSize)

af54087426605cb1acf46ef27803d2d7640ea9a8
Ellen Shapiro <designatednerd@gmail.com>
2019-05-28 10:16:59 +0200

    Update to new version of XML generator that allows generating dimen and color XML on the same line

--- code/codegen/ColorGenAndroid.kts
+++ code/codegen/ColorGenAndroid.kts
@@ -1,6 +1,6 @@
 #!/usr/bin/env kscript
 
-//DEPS org.redundent:kotlin-xml-builder:1.4.5
+//DEPS org.redundent:kotlin-xml-builder:1.5.1
 //INCLUDE ../PPPShared/src/commonMain/kotlin/ui/PPPColor.kt
 
 import org.redundent.kotlin.xml.*
@@ -19,7 +19,8 @@ val colorResXML = xml("resources") {
     }
 }
 
-val xmlString = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" + colorResXML.toString()
+val printOptions = PrintOptions(pretty = true, singleLineTextElements = true)
+val xmlString = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" + colorResXML.toString(printOptions = printOptions)
 
 val currentDirectory = File("").getAbsoluteFile()
 val colorXMLPath = File(currentDirectory, "../android/src/main/res/values/colors.xml")
--- code/codegen/DimenGenAndroid.kts
+++ code/codegen/DimenGenAndroid.kts
@@ -1,6 +1,6 @@
 #!/usr/bin/env kscript
 
-//DEPS org.redundent:kotlin-xml-builder:1.4.5
+//DEPS org.redundent:kotlin-xml-builder:1.5.1
 //INCLUDE ../PPPShared/src/commonMain/kotlin/extension/StringExtensions.kt
 //INCLUDE ../PPPShared/src/commonMain/kotlin/ui/Margin.kt
 //INCLUDE ../PPPShared/src/commonMain/kotlin/ui/FontSize.kt
@@ -26,7 +26,8 @@ val dimenResXML = xml("resources") {
     }
 }
 
-val xmlString = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" + dimenResXML.toString()
+val printOptions = PrintOptions(pretty = true, singleLineTextElements = true)
+val xmlString = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" + dimenResXML.toString(printOptions = printOptions)
 
 val currentDirectory = File("").getAbsoluteFile()
 val dimenXMLPath = File(currentDirectory, "../android/src/main/res/values/dimens.xml")
